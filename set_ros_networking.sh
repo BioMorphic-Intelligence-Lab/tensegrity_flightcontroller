a3f621beadc91ec437e3345c7abd558593645067
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2024-07-23 18:17:18 -0700

    Always set the ip starting with 169 as ros ip

--- set_ros_networking.sh
+++ set_ros_networking.sh
@@ -1,5 +1,6 @@
 # set_ros_ip.sh
 #!/bin/bash
 export ROS_MASTER_URI="http://169.254.21.103:11311"
-export ROS_IP=$(hostname -I | awk '{print $2}')
+# Choose the IP address that starts with 169
+export ROS_IP=$(hostname -I | awk '{for(i=1;i<=NF;i++) if ($i ~ /^169\./) print $i}')
 exec "$@"
